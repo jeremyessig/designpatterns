5905871aafd2cec7da8b52a8a4c80ed83e0b1df9
Jeremy <jeremy.essig@gmail.com>
2024-12-25 16:55:03 +0100

    Decorator 1 et 2

--- tests/Decorator/DecoratorTest.php
+++ tests/Decorator/DecoratorTest.php
@@ -4,15 +4,14 @@ use App\Decorator\ClientCode;
 use App\Decorator\ConcreteComponent;
 use App\Decorator\ConcreteDecoratorA;
 use App\Decorator\ConcreteDecoratorB;
+use PHPUnit\Framework\TestCase;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
-class DecoratorTest extends KernelTestCase
+class DecoratorTest extends TestCase
 {
 
     public function testClient(): void
     {
-        self::bootKernel();
-        $container = self::getContainer();
         $component = new ConcreteComponent();
         $clientCode = new ClientCode($component);
         $clientCode->doStuff();
@@ -20,8 +19,13 @@ class DecoratorTest extends KernelTestCase
         $decorator1 = new ConcreteDecoratorA($component);
         $decorator2 = new ConcreteDecoratorB($decorator1);
 
+        $clientCode1 = new ClientCode($decorator1);
+        $clientCode1->doStuff();
+
         $clientCode2 = new ClientCode($decorator2);
         $clientCode2->doStuff();
+
+
 
 
         //dump($clientCode->doStuff());
